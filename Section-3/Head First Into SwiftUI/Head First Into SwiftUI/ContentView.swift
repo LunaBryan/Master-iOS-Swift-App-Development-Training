98458183677fd567d9eaf3f6a58613344090dcba
Bryan Luna <lunabryan983@gmail.com>
2025-07-28 22:26:52 -0600

    feat: Group and fonts

--- Section-3/Head First Into SwiftUI/Head First Into SwiftUI/ContentView.swift
+++ Section-3/Head First Into SwiftUI/Head First Into SwiftUI/ContentView.swift
@@ -10,11 +10,53 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Group {
+                Text("?")
+                    .font(.system(size: 40, weight: .semibold))
+            }
+            .frame(height: 350)
+            
+            Text("Score 0")
+                .font(.system(size: 15, weight: .semibold))
+                .frame(maxWidth: .infinity, alignment: .trailing)
+            
+            Button {
+                /// Some
+            } label: {
+                Image(systemName: "cat")
+                    .symbolVariant(.fill)
+                    .foregroundStyle(.orange)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(.blue)
+                    .clipShape(.capsule)
+            }
+            
+            Button {
+                /// Some
+            } label: {
+                Image(systemName: "dog")
+                    .symbolVariant(.fill)
+                    .foregroundStyle(.brown)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(.blue)
+                    .clipShape(.capsule)
+            }
+            
+            Button {
+                /// Some
+            } label: {
+                Image(systemName: "tortoise")
+                    .symbolVariant(.fill)
+                    .foregroundStyle(.green)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 50)
+                    .background(.blue)
+                    .clipShape(.capsule)
+            }
         }
+        .frame(maxHeight: .infinity, alignment: .top)
         .padding()
     }
 }
