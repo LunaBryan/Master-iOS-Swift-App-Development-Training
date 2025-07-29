14a17d3b475caac5e236df8ced8da2f124d2d149
Bryan Luna <lunabryan983@gmail.com>
2025-07-28 22:22:43 -0600

    feat: Groups and fonts

---
+++ Section-3/Head First Into SwiftUI/Head First Into SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Head First Into SwiftUI
+//
+//  Created by Jose Luna on 7/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Section-3/Head First Into SwiftUI/Head First Into SwiftUI/Head_First_Into_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Head_First_Into_SwiftUIApp.swift
+//  Head First Into SwiftUI
+//
+//  Created by Jose Luna on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Head_First_Into_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
